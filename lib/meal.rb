320dfe3133fd74ea209a00bd5ef975eb4a2848b7
Tyler Huffman <ty.huff24@gmail.com>
2020-08-11 23:43:53 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,24 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
+
+@@all = []
+
+  def initialize(name, cusomer, total, tip)
+    @name = name
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+end
+
+
+
+
+
+
+
+
+
+
 
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,24 @@
 class Waiter
+attr_accessor :years_of_experience
+attr_reader :name
+
+@@all = []
+
+def initialize(name, years)
+  @name = name
+  @years_of_experience = years
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+
+
+
+
+
+
 
 end
