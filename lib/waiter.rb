38db821da02998aa2d000f023bf1fd735c1c0d57
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 13:54:23 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,7 +14,19 @@ def self.all
   @@all
 end
 
+def new_meal(customer, total, tip)
+  meal = Meal.new(self, customer, total, tip)
+end
 
+def meals
+  Meal.all.find_all {|meal| meal.waiter == self}
+end
+
+def best_tipper
+  highest = nil
+  Meal.all.each {|meal| 
+    if meal.tip > highest.tip
+    highest = self}
 
 
 
